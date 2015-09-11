909503aa1d7eeacec97ad7b0f32f848add206786
mofei <mofei.lh@alibaba-inc.com>
2015-09-11 17:04:59 +0800

    结构优化

--- start.js
+++ start.js
@@ -2,11 +2,30 @@
  * Created by lihui on 15-9-10.
  */
 module.exports = function(mock2easy,options ,ck){
-
-
   var fs = require('fs');
   var path = require('path');
   global._ = require('underscore');
+
+
+  if (arguments.length == 2){
+    ck = options;
+    options = _.extend({
+      port:3000,
+      lazyLoadTime:3000,
+      database:'mock2easy',
+      doc:'doc',
+      keepAlive:true,
+      isSpider:false,
+      ignoreField:[],
+      interfaceSuffix:'.json',
+      preferredLanguage:'en'
+    },mock2easy);
+    mock2easy = {
+      log: console.log,
+      error: console.error
+    }
+  }
+
 
   _.templateSettings = {
     interpolate: /\{\{(.+?)\}\}/g
@@ -43,15 +62,12 @@ module.exports = function(mock2easy,options ,ck){
     return deferred.promise;
   }
 
-
-
-
   makeDo(mock2easy,options).then(function(){
     global.options = options;
     global.language = require('./server/translate')(mock2easy,options.preferredLanguage);
 
     require('./server/cleanInterface')(mock2easy).then(function(){
-      ck(require('./app'));
+      ck(require('./app')(mock2easy,options));
     });
 
   });
