654f675a538f9ba240e1f69b72cae5c98a199b37
mofei <mofei.lh@alibaba-inc.com>
2017-02-16 16:16:53 +0800

    加强日志打印，

--- server/cleanInterface.js
+++ server/cleanInterface.js
@@ -5,7 +5,7 @@
  */
 
 
-module.exports = function(mock2easy){
+module.exports = function (mock2easy) {
   var deferred = require('q').defer();
   var fs = require('fs');
   var path = require('path');
@@ -15,77 +15,78 @@ module.exports = function(mock2easy){
   var async = require('async');
 
 
-  require('../util/rmdirSync')(mock2easy,path.resolve(global.options.doc),function(){
+  require('../util/rmdirSync')(mock2easy, path.resolve(global.options.doc), function () {
 
     var _i = 1;
     var _path = [];
 
     var _menu = global.language['SERVER-MENU-TITLE'];
-    if(!!!_filse.length){
+    if (!!!_filse.length) {
       deferred.resolve();
     }
 
-    async.each(_filse,function( file, callback) {
-        fs.readFile(file,'utf-8',function(err,data){
-            if(err){
-                mock2easy.error(err);
+    async.each(_filse, function (file, callback) {
+      fs.readFile(file, 'utf-8', function (err, data) {
+        if (err) {
+          mock2easy.error(err);
+          callback(err);
+        } else {
+          try {
+            if (data) {
+              _json = JSON.parse(data);
+              _json.interfaceUrl = _json.interfaceUrl.split('.')[0] + global.options.interfaceSuffix;
+              _json.requiredParameters.forEach(function (o) {
+                o.remark = o.rule != undefined ? o.rule : o.remark;
+                o.required = o.required != undefined ? o.required : true;
+                delete o.rule;
+              });
+              _json.responseParameters.forEach(function (o) {
+                if (o.kind == 'boolean' || o.kind == 'number') {
+                  o.rule = o.rule + '';
+                  o.kind = 'mock';
+                }
+              });
+              if (!!!_json.reqError) {
+                _json.reqError = [];
+              }
+              if (!!!_json.docError) {
+                _json.docError = [];
+              }
+              _path.push('|[' + _json.interfaceUrl + '](../' + global.options.doc + _json.interfaceUrl.replace(global.options.interfaceSuffix, '.md') + ')|' + _json.interfaceName + '|\n');
+
+              setConfiguration(mock2easy, _json).then(function () {
+                callback();
+              }, function (err) {
                 callback(err);
-            }else{
-                 try{
-                   if(data) {
-                     _json = JSON.parse(data);
-                     _json.interfaceUrl = _json.interfaceUrl.split('.')[0] + global.options.interfaceSuffix;
-                     _json.requiredParameters.forEach(function (o) {
-                       o.remark = o.rule != undefined ? o.rule : o.remark;
-                       o.required = o.required != undefined ? o.required : true;
-                       delete o.rule;
-                     });
-                     _json.responseParameters.forEach(function (o) {
-                       if (o.kind == 'boolean' || o.kind == 'number') {
-                         o.rule = o.rule + '';
-                         o.kind = 'mock';
-                       }
-                     });
-                     if (!!!_json.reqError) {
-                       _json.reqError = [];
-                     }
-                     if (!!!_json.docError) {
-                       _json.docError = [];
-                     }
-                     _path.push('|[' + _json.interfaceUrl + '](../' + global.options.doc + _json.interfaceUrl.replace(global.options.interfaceSuffix, '.md') + ')|'+_json.interfaceName+'|\n');
-
-                     setConfiguration(mock2easy, _json).then(function () {
-                       callback();
-                     },function(err){
-                       callback(err);
-                     });
-                   } else{
-                     callback();
-                   }
-                 }catch (err){
-                   mock2easy.error(err);
-                   callback(err);
-                 }
+              });
+            } else {
+              callback();
             }
-
-        });
-    },function(err){
-        if(err){
-          deferred.reject(err);
-        }else {
-          _path.sort().forEach(function(o,i){
-            _menu += '|'+ (i+1) + '. ' + o;
-          });
-
-          require('../util/createFile')(path.resolve(global.options.doc)+'/menu.md',_menu,mock2easy).then(function(){
-            deferred.resolve();
-          },function(err){
-            deferred.reject(err);
-          });
+          } catch (err) {
+            mock2easy.error(file + '出现异常');
+            mock2easy.error(err);
+            callback(err);
+          }
         }
+
+      });
+    }, function (err) {
+      if (err) {
+        deferred.reject(err);
+      } else {
+        _path.sort().forEach(function (o, i) {
+          _menu += '|' + (i + 1) + '. ' + o;
+        });
+
+        require('../util/createFile')(path.resolve(global.options.doc) + '/menu.md', _menu, mock2easy).then(function () {
+          deferred.resolve();
+        }, function (err) {
+          deferred.reject(err);
+        });
+      }
     });
 
-});
+  });
 
   return deferred.promise;
 }
